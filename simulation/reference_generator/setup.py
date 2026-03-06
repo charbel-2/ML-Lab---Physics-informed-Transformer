1620c15958a5eb8247ce6b0334bdc461f5c8b4b5
charbel-2 <131307737+charbel-2@users.noreply.github.com>
2026-03-06 14:33:21 +0100

    Add files via upload

---
+++ simulation/reference_generator/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'reference_generator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'rclpy', 'std_msgs'],
+    zip_safe=True,
+    maintainer='charbel',
+    maintainer_email='charbel@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'reference_generator = reference_generator.reference_generator:main',
+        ],
+    },
+
+)
+
